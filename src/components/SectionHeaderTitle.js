cc78c1981db85a6d1a19e5fb6c6d0d66ab55d316
fancykingstar <fancykingstar2019@gmail.com>
2019-10-31 13:41:21 -0700

    fixed break points, margins and grid gap for home page

--- src/components/SectionHeaderTitle.js
+++ src/components/SectionHeaderTitle.js
@@ -6,7 +6,7 @@ import '../css/SectionHeaderTitle.css';
 class SectionHeaderTitle extends Component {
 	render() {
 		return (
-			<div className="section-title-container">
+			<div className={"section-title-container" + (this.props.leftAligned ? " left-aligned-section-header" : "")}>
 				<div className={"title section-header-content"
 					+ (this.props.leftAligned ? " left-aligned-section-header" : "") 
 					+ (this.props.smallWindow ? " section-header-small" : "")}>{this.props.title}</div>
