7ce0438880c2029b497bf15ff55c0948954b389f
fancykingstar <fancykingstar2019@gmail.com>
2019-11-01 16:29:39 -0700

    css - header height, pop up margin

--- src/components/RoadmapVertical.js
+++ src/components/RoadmapVertical.js
@@ -71,7 +71,7 @@ export function RoadmapVertical(props) {
                     </div>
                     {selectedContent.content.map(content => (
                         <div key={content.item + "-" + selectedContent.title} className="roadmap-item">
-                            <img className="item-bullet" src={renderIcon("triangleBulletOrange")} alt=""></img>
+                            <img className="item-bullet" src={renderIcon("roundBullet")} alt=""></img>
                             <label className="item-label">{content.item}</label>
                         </div>
                     ))}
